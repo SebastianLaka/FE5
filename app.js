cf72e87785044e2a4c0f310662c3660346beb218
Sebastian <sebastianlakla@gmail.com>
2023-09-03 14:47:23 +0200

    Get elements from DOM and create function

---
+++ app.js
@@ -0,0 +1,10 @@
+const faqQuestion = document.querySelector('.faq-question');
+const faqAnswer = document.querySelector('.faq-question__answer');
+console.log(faqQuestion, faqAnswer);
+
+faqQuestion.addEventListener('click', function(){
+    
+    // faqAnswer.style.display = 'block';
+})
+
+
