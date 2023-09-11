6d5e8c4271ae7947180657ae8e377f1c9175a28b
Sebastian <sebastianlakla@gmail.com>
2023-09-11 21:42:11 +0200

    add toggle class at accordion area

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-const faqQuestion = document.querySelector('.faq-question');
-const faqAnswer = document.querySelector('.faq-question__answer');
-console.log(faqQuestion, faqAnswer);
+"use strict";
+const accordion = document.querySelectorAll("accordion-area");
 
-faqQuestion.addEventListener('click', function(){
-    
-    // faqAnswer.style.display = 'block';
-})
-
-
+accordion.forEach((showAnswer) => {
+  showAnswer.addEventListener("click", () => {
+    showAnswer.classList.toggle("show-answer");
+    showAnswer.classList.toggle("rotate-arrow");
+  });
+});
