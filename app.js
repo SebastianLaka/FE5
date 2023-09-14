c5db090c443f8a07cbba3cafec8a00facf1ac926
Sebastian <sebastianlakla@gmail.com>
2023-09-14 21:12:33 +0200

    corrected wrong classes

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 "use strict";
-const accordion = document.querySelectorAll("accordion-area");
-
+const accordion = document.querySelectorAll(".accordion-area");
+console.log(accordion);
 accordion.forEach((showAnswer) => {
   showAnswer.addEventListener("click", () => {
     showAnswer.classList.toggle("show-answer");
-    showAnswer.classList.toggle("rotate-arrow");
   });
 });
