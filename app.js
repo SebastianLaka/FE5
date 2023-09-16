7bd04a097f6afbcbf6356c3580127d8d899e2a5a
Sebastian <sebastianlakla@gmail.com>
2023-09-16 22:16:24 +0200

    add function at first element at carousell

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 "use strict";
-const accordion = document.querySelectorAll(".accordion-area");
-console.log(accordion);
-accordion.forEach((showAnswer) => {
-  showAnswer.addEventListener("click", () => {
-    showAnswer.classList.toggle("show-answer");
-  });
+const accordions = document.querySelectorAll('.faq-area');
+console.log(accordions);
+accordions.forEach(accordion => {
+  accordion.addEventListener("click", () => {
+  const faqQuestion = document.querySelectorAll(".faq-question")
+  faqQuestion.forEach((showAnswer, rotateArrow) =>{
+    showAnswer = document.querySelector('.faq-question__answer')
+    showAnswer.classList.toggle('show-answer')
+    rotateArrow = document.querySelector(".faq-question__arrow-icon")
+    rotateArrow.classList.toggle('rotate-arrow')
+  })
+  
 });
+});
+  // const arrowIconPlace = document.querySelector(".faq-question__arrow-icon")
+  // console.log(faqQuestion, arrowIconPlace);
+  // faqQuestion.forEach(answer =>{
+  //   answer.classList.toggle('show-answer')
+  // })
+// answer.classList.toggle('show-answer')
+// rotate.classList.toggle('rotate-arrow')
