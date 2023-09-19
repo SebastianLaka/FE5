fbd4321d33eee8b537922d271ba97d2c354d09b0
Sebastian <sebastianlakla@gmail.com>
2023-09-19 19:59:11 +0200

    Add sizes for arrow icons, add style for question class. Add toggle function and create setTimeout. Finaly end project.

--- app.js
+++ app.js
@@ -9,17 +9,17 @@
 // });
 
 const accordions = document.querySelectorAll('.accordion-area');
-
+let timeOut;
 accordions.forEach((accordion) => {
   accordion.addEventListener("click", () => {
+    timeOut = setTimeout(() => {
     const question = accordion.querySelector('.faq-question__question')
-    question.style.color = 'black';
+    question.classList.toggle('question-active')
     const answer = accordion.querySelector('.faq-question__answer')
     answer.classList.toggle('show-answer')
-    
     const arrowIcon = accordion.querySelector(".faq-question__arrow-icon")
     arrowIcon.classList.toggle('rotate-arrow')
-
+    }, 300)
 });
 });
 
