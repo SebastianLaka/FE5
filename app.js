83ce386676679475155a8cb4c827500ddc38befa
Sebastian <sebastianlakla@gmail.com>
2023-09-20 21:32:01 +0200

    corrected path with icons and background image

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
 "use strict";
-// const accordion = document.querySelectorAll('.faq-area');
-// console.log(accordion);
-// accordion.forEach((showAnswer) => {
-//   showAnswer.addEventListener("click", () => {
-//     showAnswer.classList.toggle("show-answer");
-//     showAnswer.classList.toggle('rotate-arrow')
-//   });
-// });
-
 const accordions = document.querySelectorAll('.accordion-area');
 let timeOut;
-accordions.forEach((accordion) => {
+accordions.forEach(accordion => {
   accordion.addEventListener("click", () => {
     timeOut = setTimeout(() => {
-    const question = accordion.querySelector('.faq-question__question')
-    question.classList.toggle('question-active')
-    const answer = accordion.querySelector('.faq-question__answer')
-    answer.classList.toggle('show-answer')
-    const arrowIcon = accordion.querySelector(".faq-question__arrow-icon")
-    arrowIcon.classList.toggle('rotate-arrow')
+    const question = accordion.querySelector('.faq-question__question');
+    question.classList.toggle('question-active');
+    const answer = accordion.querySelector('.faq-question__answer');
+    answer.classList.toggle('show-answer');
+    const arrowIcon = accordion.querySelector(".faq-question__arrow-icon");
+    arrowIcon.classList.toggle('rotate-arrow');
     }, 300)
 });
 });
-
-  // faqQuestion.forEach(answer =>{
-  //   answer.classList.toggle('show-answer')
-  // })
-// answer.classList.toggle('show-answer')
-// rotate.classList.toggle('rotate-arrow')
